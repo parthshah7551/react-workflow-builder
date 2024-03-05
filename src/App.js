657ab7802897548f46ef2e83dba6d338d36928ed
Parth Shah <parth.shah@edeltaes.com>
2024-03-05 21:34:57 +0530

    feat: initial react flow setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import OverviewFlow from "./component/OverviewFlow";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <OverviewFlow />
+    </div>
+  );
 }
 
 export default App;
