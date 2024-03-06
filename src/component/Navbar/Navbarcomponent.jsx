a0a0b2763b3a3659a59acbe0e589333e7a7a72ca
Parth Shah <parth.shah@edeltaes.com>
2024-03-06 17:30:16 +0530

    feat: csv file read using react js

--- src/component/Navbar/Navbarcomponent.jsx
+++ src/component/Navbar/Navbarcomponent.jsx
@@ -1,35 +1,52 @@
-import React, { useState } from "react";
-import Select from "react-select";
-import { CSV_OPTIONS } from "../../constant";
+import React from "react";
 import { Button } from "react-bootstrap";
 import PropTypes from "prop-types";
+import Papa from "papaparse";
 // import { useDispatch, useSelector } from "react-redux";
 // import { counterReducer } from "../../redux/reducer";
 // import { Button, Form } from "react-bootstrap";
 
 const Navbarcomponent = ({ saveModal }) => {
-  const [csvFileName, setCSVFileName] = useState("");
+  // const [csvFileName, setCSVFileName] = useState("");
   // const dispatch = useDispatch();
   // const data = useSelector((state) => {
   //   console.log("state: ", state);
   //   return state;
   // });
+  const handleFileUpload = (event) => {
+    const file = event?.target?.files[0]; // Check if event object and target property exist
+    console.log("file: ", file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const csvData = event.target.result;
+        Papa.parse(csvData, {
+          complete: (result) => {
+            console.log("Parsed CSV data:", result.data);
+            // Handle parsed CSV data here
+          },
+          header: true, // Set to true if CSV file contains headers
+        });
+      };
+      reader.readAsText(file);
+    }
+  };
   return (
     <div className="navbarContainer d-flex justify-content-between">
       <div className="d-flex">
-        <Select
-          className="m-2"
-          name="otherLoanDetails"
-          id="otherLoanDetails"
-          value={csvFileName}
-          onChange={(selectedotherLoanDetailsOption) => {
-            setCSVFileName(selectedotherLoanDetailsOption);
-          }}
-          placeholder="Select CSV"
-          options={CSV_OPTIONS}
-          isSearchable
-          isClearable
-        />
+        <Button variant="secondary" className="m-2">
+          <label htmlFor="fileSelect" style={{ margin: 0 }}>
+            Select CSV File
+          </label>
+          <input
+            type="file"
+            id="fileSelect"
+            style={{ display: "none" }}
+            accept=".csv"
+            onChange={(e) => handleFileUpload(e)}
+          />
+        </Button>
         <Button variant="primary" className="m-2">
           Dashboard
         </Button>
